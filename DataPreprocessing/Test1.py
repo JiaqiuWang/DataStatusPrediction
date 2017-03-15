3d7d423e4ffbcb81b254b13fbe85ed146d50d631
wangjiaqiu <wangjiaqiu186@icloud.com>
2017-03-15 22:55:07 +0800

    插入数据库MongoDB

--- DataPreprocessing/Test1.py
+++ DataPreprocessing/Test1.py
@@ -3,21 +3,33 @@
 """
 import time
 import pandas as pd
-import DataPreprocessing.Test4 as t4
+import DataPreprocessing.Test4 as dpt4
+import pymongo
 
 
 class DataProcess:
 
-    # 类公有变量；IP地址或者写127.0.0.1
+    # 类公有变量
+    nan_list = []  # 存放空值行的队列
 
     # 构造函数
-    def __init__(self, file_path):
+    def __init__(self, file_path, db_name, collection_name, ip_address):
         self.file_path = file_path
+        self.db_name = db_name
+        self.collection_name = collection_name
+        self.ip_address = ip_address
+        # 全局变量-连接指定IP地址的数据库
+        self.client = pymongo.MongoClient(self.ip_address, 27017)
+        # 获取数据库
+        self.db = self.client.get_database(self.db_name)
+        # 获取集合
+        self.collection = self.db.get_collection(self.collection_name)
 
     # 析构函数
     def __del__(self):
         class_name = self.__class__.__name__
-        print(class_name, "销毁")
+        self.client.close()
+        print(class_name, "Destroy.")
 
 # ---------------------------------------------------------------------------------------
 
@@ -25,44 +37,139 @@ class DataProcess:
     def read_file(self):
         data = pd.read_csv(self.file_path, encoding='utf-8')
         print("data:")
-        # 逐个元素判断是否为空值
+        # 逐个元素判断是否为空值,将空值行，放入一个队列中
         for i in range(len(data)):
             print("i:", i)
-            # print("row: ", data.iloc[i])
+            user_id = ""  # 用户ID
+            service_id = ""  # 服务ID
+            date_time = ""  # 格式化时间
+            time_stamp = ""  # 时间戳
+            activity = ""  # 行为
+            content = ""  # 内容
+            key_words = []  # 内容关键词（自然语言做的分词）
+            title_text = ""  # 标题
+            star_badge = ""  # 奖励
+            row = data.iloc[i]
+            # print("type:", type(row), "row: ", row)
             for j in data.columns:
                 j = str(j)
-                if str(data.iloc[i][j]) == "nan":
-                    print("空值", data.iloc[i], j, "element：", data.iloc[i][j])
-                    continue
+                # if str(data.iloc[i][j]) == "nan":
+                #     print("空值", row, j, "element：", data.iloc[i][j])
+                #     self.nan_list.append(row)
+                #     continue
                 element = str(data.iloc[i][j])
-                print(j, ":", element)
+                if j == "用户ID":
+                    user_id = element
+                    print(j, ":", user_id)
+                if j == "服务ID":
+                    service_id = element
+                    print(j, ":", service_id)
                 if j == "时间":
+                    if str(data.iloc[i][j]) == "nan":
+                        # print("空值", row, j, "element：", data.iloc[i][j])
+                        self.nan_list.append(row)
+                        continue
                     element = element.replace("Z", "")
-                    print("j':", element)
+                    date_time = element
+                    print(j, ":", date_time)
                     # 将格式化时间转换成时间戳10位
                     # 1中间过程，一般都需要将字符串转化为时间数组
                     timeArray = time.strptime(element, "%Y-%m-%d %H:%M:%S")
                     # 2将"2011-09-28 10:00:00"转化为时间戳
                     timestamp = int(time.mktime(timeArray))
-                    print("timestamp:", timestamp)
+                    print("timestamp:", timestamp, " type:", type(timestamp))
+                    time_stamp = timestamp
+                if j == "行为":
+                    activity = element
+                    print(j, ":", activity)
                 if j == "内容":
-                    content = t4.main(element)
-                    print("key words list:", content)
-
+                    content = element
+                    print(j, ":", content)
+                    temp_keywords = dpt4.main(element)   # keywords是一个List结构
+                    print("keywords:", temp_keywords)
+                    key_words = key_words + temp_keywords
+                if j == "title":
+                    title_text = element
+                    print(j, ":", title_text)
+                    temp_keywords = dpt4.main(title_text)  # keywords是一个List结构
+                    print("keywords:", temp_keywords)
+                    key_words = key_words + temp_keywords
+                if j == "标记":
+                    star_badge = element
+                    print(j, ":", star_badge)
+                # end if
+            # 输出每一行的input_list
+            insert_text = {"uid": self.collection_name, "用户ID": user_id,
+                           "服务ID": service_id, "时间": date_time,
+                           "timestamp": time_stamp, "activity": activity, "内容": content,
+                           "keywords": key_words, "title": title_text, "标记": star_badge}
+            print("row_input_list:", insert_text)
+            # 插入数据库
+            self.input_database(insert_text)
             print()
+        # end for, 判断是不是有空值的元组
+        if self.nan_list:
+            for var_nan in self.nan_list:
+                print("NaN row:", var_nan)
+            print("空值的个数：", len(self.nan_list))
 
 # ---------------------------------------------------------------------------------------
 
+    """
+    数据库操作
+    """
+    # 写入MongoDB数据库
+    def input_database(self, input_tuple):
+        # 添加单条记录到集合中
+        self.collection.insert(input_tuple)
 
+    # 批量插入数据
+    def input_many_database(self, input_tuple):
+            # 添加单条记录到集合中
+            self.collection.insert_many(input_tuple)
 
+    # 查询所有collections
+    def find_all(self):
+        # 查询所有
+        for data in self.collection.find():
+            print(data)
+        print("总记录数为：", self.collection.find().count())
+        self.client.close()
+
+    # 更新数据库
+    def update_database(self):
+        self.collection.update({"_id": 1, "pPer_id": "p2"},
+                               {"$set": {"pName": "卫国"}})
+        self.client.close()
+
+    # 查找一个文档
+    def find_one(self):
+        for u in self.collection.find({"_id": 1}):
+            print("符合条件的记录(_id:1 ): ", u)
+            self.client.close()
+
+    # 删除一个数据
+    def delete_one(self):
+        for u in self.collection.find({"_id": 1}):
+            print("符合条件的记录(_id:1 ): ", u)
+            self.collection.remove({"_id": 1})
+            self.client.close()
+
+    # 删除所有数据
+    def delete_all(self):
+        self.collection.remove()
+        self.client.close()
 
 # ----------------------------------------------------------------------------------------------------------
 
 
 def main_operation():
     """Part1: 初始化参数"""
-    file_path = 'data/answered2.csv'  # 文件路径和文件名
-    dp1 = DataProcess(file_path)
+    file_path = 'data/校订.csv'  # 读取文件路径和文件名
+    ip_address = "127.0.0.1"  # 主机IP地址
+    db_name = "predictionData"  # 数据库名字
+    collection_name = "U01"  # 集合的名字
+    dp1 = DataProcess(file_path, db_name, collection_name, ip_address)
     dp1.read_file()
 
 if __name__ == "__main__":
